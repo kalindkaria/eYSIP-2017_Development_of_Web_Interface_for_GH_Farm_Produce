c9246be7c3dd09c17d088d31e512b01cc0ed4a7a
keivan-shah <keivanshahh@gmail.com>
2017-05-29 10:02:25 +0530

    Started django project ghfarm, added scripts for server setup

--- raspberrypi/setup.sh
+++ raspberrypi/setup.sh
@@ -4,7 +4,7 @@ cat client.py >> /home/pi/ghfarm/client.py
 cat ghfarm.py >> /home/pi/ghfarm/ghfarm.py
 chmod +x /home/pi/ghfarm/client.py
 chmod +x /home/pi/ghfarm/ghfarm.py
-sudo apt-get install -y python3 python3-rpi.gpio
+sudo apt-get install -y python3 python3-rpi.gpio fswebcam
 crontab -l >> mycron
 echo '@reboot /home/pi/ghfarm/ghfarm.py'>> mycron
 echo '@reboot /home/pi/ghfarm/client.py'>> mycron
