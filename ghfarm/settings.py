3773b6955bf5cafaa55b765932e813bcd7efaa6c
hemang97 <gandhihemang97@gmail.com>
2017-05-30 11:07:51 +0530

    Connected django with mysql database

--- ghfarm/settings.py
+++ ghfarm/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'ghfarm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ghfarm',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
     }
 }
 
